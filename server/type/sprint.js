f79f88e2aefcebae2ee11439bc2d74b0353071ea
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-12 09:58:15 +0200

    :rocket: made mocked data consistent

--- server/type/sprint.js
+++ server/type/sprint.js
@@ -9,16 +9,18 @@ const {
 
 const _ = require('lodash');
 
+
 const users = [
   { id: '1', username: 'igi', password: 'catbeef', points: 4, },
-  { id: '2', username: 'aga', password: 'dogsausage', points: 2, },
-  { id: '3', username: 'grzesiek', password: 'iphonbeef', points: 0, },
-  { id: '4', username: 'darek', password: 'applesausage', points: 0, },
+  { id: '2', username: 'aga', password: 'dogsausage', points: 4, },
+  { id: '3', username: 'natalka', password: 'fishsandwich', points: 4, },
+  { id: '4', username: 'grzesiek', password: 'iphonbeef', points: 0, },
+  { id: '5', username: 'darek', password: 'applesausage', points: 0, },
 ];
 
 const tasks = [
-  { id: '1', title: 'zmywanie podlogi', reward: 3, iconIndex: 0, },
-  { id: '2', title: 'mycie lodowki', reward: 4, iconIndex: 1, },
+  { id: '1', title: 'zrobienie apki', reward: 3, iconIndex: 0, },
+  { id: '2', title: 'mycie podłogi', reward: 4, iconIndex: 1, },
 ];
 
 const doneTasks = [
@@ -32,12 +34,28 @@ const doneTasks = [
     user: users[1],
     task: tasks[1],
   },
+  { id: '3',
+    date: 'dzisiaj',
+    user: users[3],
+    task: tasks[1],
+  },
+  { id: '4',
+    date: 'wczoraj',
+    user: users[4],
+    task: tasks[1],
+  },
 ];
 
 const sprints = [
-  { id: '1', prize: 'piwo', deadline: '4 lipca', doneTasks: doneTasks, },
-  { id: '2', prize: 'wodka', deadline: '14 stycznia', doneTasks: doneTasks, },
+  { id: '1', prize: 'piwo', deadline: '4 lipca', doneTasks: [doneTasks[0], doneTasks[1], ], },
+  { id: '2', prize: 'wodka', deadline: '14 stycznia', doneTasks: [doneTasks[2], doneTasks[3], ], },
 ];
+
+const rooms = [
+  { id: '1', roommates: [users[0], users[1], ], sprint: sprints[0], },
+  { id: '2', roommates: [users[2], users[3], ], sprint: sprints[1], },
+];
+
 
 const SprintType = new GraphQLObjectType({
   name: 'Sprint',
