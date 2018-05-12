5babfba33001955e9a9625fdca82194074d0aa1c
radlinskii <radlinsk@student.agh.edu.pl>
2018-05-12 14:32:30 +0200

    :rocket: mocked deadlines to real dates format

--- server/type/room.js
+++ server/type/room.js
@@ -46,7 +46,7 @@ const doneTasks = [
 ];
 
 const sprints = [
-  { id: '1', prize: 'piwo', deadline: '7/13/2010', doneTasks: [doneTasks[0], doneTasks[1], ], },
+  { id: '1', prize: 'piwo', deadline: '7/13/2018', doneTasks: [doneTasks[0], doneTasks[1], ], },
   { id: '2', prize: 'wodka', deadline: '7/29/2018', doneTasks: [doneTasks[2], doneTasks[3], ], },
 ];
 
